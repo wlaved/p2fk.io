ea3aeae681d2a50a086b8c29f825c0e2c3075c51
embiimob <embii@embii.com>
2024-05-07 15:37:30 -0500

    p2fk.io v0.1.0-beta pre release

---
+++ Controllers/GetKeywordByPublicAddressController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.RegularExpressions;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace p2fk.io.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class GetKeywordByPublicAddressController : ControllerBase
+    {
+
+        // GET <GetKeywordByPublicAddressController >/5
+        [HttpGet("{address}")]
+        public ActionResult Get(string address)
+        {
+            // Regular expression for cryptocurrency address validation
+            string pattern = @"^[a-zA-Z0-9][a-km-zA-HJ-NP-Z1-9]{25,34}$";
+            if (Regex.IsMatch(address, pattern))
+            {
+
+                Wrapper wrapper = new Wrapper();
+
+                string arguments = "";
+                string result = "";
+                               
+                    arguments = "--getkeywordbypublicaddress --address " + address;
+                    result = wrapper.RunCommand(wrapper.ProdCLIPath, arguments);
+              
+
+                return Content(result, "application/json");
+            }
+            else { return Content("[\"invalid address format\"]", "application/json"); }
+
+        }
+
+
+    }
+}
---
+++ Controllers/GetPublicAddressByKeywordController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.RegularExpressions;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace p2fk.io.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class GetPublicAddressByKeywordController : ControllerBase
+    {
+
+        // GET <GetPublicAddressByKeywordController>/5
+        [HttpGet("{keyword}")]
+        public ActionResult Get(string keyword, bool mainnet = true)
+        {
+           
+                Wrapper wrapper = new Wrapper();
+
+                string arguments = "";
+                string result = "";
+
+                if (mainnet)
+                {
+                    arguments = "--versionbyte " + wrapper.ProdVersionByte + " --getpublicaddressbykeyword --keyword " + keyword;
+                    result = wrapper.RunCommand(wrapper.ProdCLIPath, arguments);
+                }
+                else { arguments = "--versionbyte " + wrapper.TestVersionByte + " --getpublicaddressbykeyword --keyword " + keyword;
+                    result = wrapper.RunCommand(wrapper.TestCLIPath, arguments);
+                }
+
+
+                return Content(result, "application/json");
+            
+
+        }
+
+
+    }
+}
